da660bb6407b6ba0e37047bc420a19addbc2ad86
Yann Bernard <yannbernard9@icloud.com>
2022-12-15 11:53:41 +0100

    has won is workin inefficient

--- src/main/java/de/uni/hannover/swt/logic/Game.java
+++ src/main/java/de/uni/hannover/swt/logic/Game.java
@@ -11,6 +11,8 @@ public class Game {
     private final Board _board;
     private Player _turn;
 
+    private WinInfo _won;
+
     public Game() {
         _board = new Board();
         _turn = Player.OPLAYER;
@@ -26,6 +28,7 @@ public class Game {
         if (!success) return;
 
         _turn = _turn == Player.OPLAYER ? Player.XPLAYER : Player.OPLAYER;
+//        winCheck();
     }
 
     public Player getPlayer() {
@@ -35,6 +38,67 @@ public class Game {
     private Player correspondingPlayer(EnumMarks mark) {
         return mark == EnumMarks.OMARK ? Player.OPLAYER: Player.XPLAYER;
     }
+
+//    private void winCheck() {
+//        var tmpField = _board.getFields();
+//
+//        /* horizontal check */
+//        for (int i = 0; i < MATRIX_SIZE; i++) {
+//            var distinctArr = Arrays.stream(tmpField[i]).distinct();
+//            if (distinctArr.count() == 1 && tmpField[i][0] != EnumMarks.EMPTY) {
+//                var mark = tmpField[i][0];
+//                Board tmpBoard = new Board();
+//                for (int place = 0; place < MATRIX_SIZE; place++)
+//                    tmpBoard.setField(i, place, mark);
+//                System.out.println("WINNER:-----------:\n" + tmpBoard);
+//                _won = new WinInfo(true, this.correspondingPlayer(mark), tmpBoard.getFields());
+//            }
+//        }
+//
+//        /* vertical check */
+//        for (int i = 0; i < MATRIX_SIZE; i++) {
+//            EnumMarks[] checkArr = new EnumMarks[MATRIX_SIZE];
+//            for (int j = 0; j < MATRIX_SIZE; j++) checkArr[j] = tmpField[j][i];
+//
+//            var distinctArr = Arrays.stream(checkArr).distinct();
+//            if (distinctArr.count() == 1 && checkArr[0] != EnumMarks.EMPTY) {
+//                Board tmpBoard = new Board();
+//                var mark = checkArr[0];
+//
+//                for (int j = 0; j < MATRIX_SIZE; j++) tmpBoard.setField(j, i, mark);
+//                System.out.println("WINNER:-----------:\n" + tmpBoard);
+//                _won = new WinInfo(true, this.correspondingPlayer(mark), tmpBoard.getFields());
+//            }
+//        }
+//
+//        /* diagonal check */
+//        var diag1 = tmpField[0][0];
+//        var diag2 = tmpField[0][2];
+//
+//        Board tmpBoard = new Board();
+//        if (diag1 != EnumMarks.EMPTY) {
+//            if (diag1 == tmpField[1][1] && diag1 == tmpField[2][2]) {
+//                tmpBoard.setField(0, 0, diag1);
+//                tmpBoard.setField(1, 1, diag1);
+//                tmpBoard.setField(2, 2, diag1);
+//                System.out.println("WINNER:-----------:\n" + tmpBoard);
+//                _won =  new WinInfo(true, this.correspondingPlayer(diag1), tmpBoard.getFields());
+//            }
+//        }
+//
+//        if (diag2 != EnumMarks.EMPTY) {
+//            if (diag2 == tmpField[1][1] && diag2 == tmpField[2][0]) {
+//                tmpBoard.setField(0, 2, diag2);
+//                tmpBoard.setField(1, 1, diag2);
+//                tmpBoard.setField(2, 0, diag2);
+//                System.out.println("WINNER:-----------:\n" + tmpBoard);
+//                _won = new WinInfo(true, this.correspondingPlayer(diag2), tmpBoard.getFields());
+//            }
+//        }
+//
+//        /* If all checks fail return false*/
+//        _won = new WinInfo(false, Player.NONE, null);
+//    }
 
     public WinInfo hasWon() {
         var tmpField = _board.getFields();
@@ -55,14 +119,45 @@ public class Game {
         /* vertical check */
         for (int i = 0; i < MATRIX_SIZE; i++) {
             EnumMarks[] checkArr = new EnumMarks[MATRIX_SIZE];
-            for (int j = 0; j < MATRIX_SIZE; j++) checkArr[j] = tmpField[i][j];
+            for (int j = 0; j < MATRIX_SIZE; j++) checkArr[j] = tmpField[j][i];
 
             var distinctArr = Arrays.stream(checkArr).distinct();
-//            if (distinctArr.count() == 1 && tmpField[i][0] != EnumMarks.EMPTY) {
-//
-//            }
+            if (distinctArr.count() == 1 && checkArr[0] != EnumMarks.EMPTY) {
+                Board tmpBoard = new Board();
+                var mark = checkArr[0];
+
+                for (int j = 0; j < MATRIX_SIZE; j++) tmpBoard.setField(j, i, mark);
+                System.out.println("WINNER:-----------:\n" + tmpBoard);
+                return new WinInfo(true, this.correspondingPlayer(mark), tmpBoard.getFields());
+            }
         }
 
+        /* diagonal check */
+        var diag1 = tmpField[0][0];
+        var diag2 = tmpField[0][2];
+
+        Board tmpBoard = new Board();
+        if (diag1 != EnumMarks.EMPTY) {
+            if (diag1 == tmpField[1][1] && diag1 == tmpField[2][2]) {
+                tmpBoard.setField(0, 0, diag1);
+                tmpBoard.setField(1, 1, diag1);
+                tmpBoard.setField(2, 2, diag1);
+                System.out.println("WINNER:-----------:\n" + tmpBoard);
+                return new WinInfo(true, this.correspondingPlayer(diag1), tmpBoard.getFields());
+            }
+        }
+
+        if (diag2 != EnumMarks.EMPTY) {
+            if (diag2 == tmpField[1][1] && diag2 == tmpField[2][0]) {
+                tmpBoard.setField(0, 2, diag2);
+                tmpBoard.setField(1, 1, diag2);
+                tmpBoard.setField(2, 0, diag2);
+                System.out.println("WINNER:-----------:\n" + tmpBoard);
+                return new WinInfo(true, this.correspondingPlayer(diag2), tmpBoard.getFields());
+            }
+        }
+
+        /* If all checks fail return false*/
         return new WinInfo(false, Player.NONE, null);
     }
 
