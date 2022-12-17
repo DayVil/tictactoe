7697d1fd617debeeacea805c1c90e83c1b550865
Marc Drewing <mldrewing@googlemail.com>
2022-12-17 16:54:12 +0100

    added a Label to show which players turn it is and added some comments

--- src/main/java/de/uni/hannover/swt/GUI/Control.java
+++ src/main/java/de/uni/hannover/swt/GUI/Control.java
@@ -3,19 +3,39 @@ package de.uni.hannover.swt.GUI;
 import de.uni.hannover.swt.logic.IGame;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+/*
+Adds the control to the MVC Pattern.
+ */
+
 public class Control extends JPanel {
 
-    private final View board;
+    private final View board; //The view which the control is connected to in the mvc pattern
+    private JLabel nextPlayerLabel;
 
     public Control(View b) {
-        board = b;
+        setPreferredSize(new Dimension(550, 600));
 
-        JPanel panel = new JPanel();
-        panel.add(board);
-        add(panel);
+        board = b; //The constructor takes the view to control as a parameter and saves it to the attributes of the object
+
+        nextPlayerLabel = new JLabel("nextPlayer:");
+        //nextPlayerLabel.setVerticalTextPosition(JLabel.BOTTOM);
+        //nextPlayerLabel.setHorizontalTextPosition(JLabel.LEFT);
+        //nextPlayerLabel.setLocation(200,400);
+        setLayout(new BorderLayout());
+        add(nextPlayerLabel, BorderLayout.SOUTH);
+        nextPlayerLabel.setText(String.valueOf(board.getCurrentGame().getPlayer()));
+
+        JPanel controlPanel = new JPanel();
+
+
+        controlPanel.add(board); //adds the view to the panel
+        add(controlPanel);
+
+
 
         board.addMouseListener(new MouseAdapter() {
             @Override
@@ -31,5 +51,6 @@ public class Control extends JPanel {
         IGame game = board.getCurrentGame();
         game.setPoint(r, c);
         board.setCurrentGame(game);
+        nextPlayerLabel.setText(String.valueOf(game.getPlayer())); //Takes the Player of the new Turn and changes the JLabel to show the name
     }
 }
--- src/main/java/de/uni/hannover/swt/GUI/View.java
+++ src/main/java/de/uni/hannover/swt/GUI/View.java
@@ -17,20 +17,22 @@ import static de.uni.hannover.swt.App.MATRIX_SIZE;
 public class View extends JPanel {
 
     private IGame currentGame;
-    private BufferedImage board, redX, blueCircle, check;
+    private BufferedImage gridImage, redX, blueCircle, check;
     protected final int UNIT = 170;
 
+
     /**
-     * Creats a new View which displays the Board which shows a new "Game".
+     * Creates a new View which displays the Board which shows a new "Game".
      */
     public View() {
         currentGame = new Game();
         loadImages();
+
     }
 
     private void loadImages() {
         try {
-            board = ImageIO.read(getClass().getClassLoader().getResourceAsStream("board.png"));
+            gridImage = ImageIO.read(getClass().getClassLoader().getResourceAsStream("gridImage.png"));
             redX = ImageIO.read(getClass().getClassLoader().getResourceAsStream("redX.png"));
             blueCircle = ImageIO.read(getClass().getClassLoader().getResourceAsStream("blueCircle.png"));
         } catch (IOException e) {
@@ -41,7 +43,7 @@ public class View extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(board, 0, 0, null);
+        g.drawImage(gridImage, 0, 0, null);
 
         for (byte r = 0; r < MATRIX_SIZE; r++) {
             for (byte c = 0; c < MATRIX_SIZE; c++) {
