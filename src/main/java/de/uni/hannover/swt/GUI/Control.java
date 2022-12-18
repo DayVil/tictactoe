988d230c4f01723804a5921925220a0a114a4341
Marc Drewing <mldrewing@googlemail.com>
2022-12-18 11:23:21 +0100

    Changed variable names for understandability

--- src/main/java/de/uni/hannover/swt/GUI/Control.java
+++ src/main/java/de/uni/hannover/swt/GUI/Control.java
@@ -16,15 +16,15 @@ public class Control extends JPanel {
     private final View board; //The view which the control is connected to in the mvc pattern
     private JLabel nextPlayerLabel;
 
+    private final int WINDOW_WIDTH = 550, WINDOW_HEIGHT = 600;
+
+
     public Control(View b) {
-        setPreferredSize(new Dimension(550, 600));
+        setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
 
         board = b; //The constructor takes the view to control as a parameter and saves it to the attributes of the object
 
         nextPlayerLabel = new JLabel("nextPlayer:");
-        //nextPlayerLabel.setVerticalTextPosition(JLabel.BOTTOM);
-        //nextPlayerLabel.setHorizontalTextPosition(JLabel.LEFT);
-        //nextPlayerLabel.setLocation(200,400);
         setLayout(new BorderLayout());
         add(nextPlayerLabel, BorderLayout.SOUTH);
         nextPlayerLabel.setText(String.valueOf(board.getCurrentGame().getPlayer()));
@@ -40,8 +40,8 @@ public class Control extends JPanel {
         board.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                int c = (int) (e.getPoint().getX() / board.UNIT);
-                int r = (int) (e.getPoint().getY() / board.UNIT);
+                int c = (int) (e.getPoint().getX() / board.POS_SCALING_FACTOR);
+                int r = (int) (e.getPoint().getY() / board.POS_SCALING_FACTOR);
                 whenMousePressed(c, r);
             }
         });
