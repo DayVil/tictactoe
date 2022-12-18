2f891d5c148312dda717c10dc9504ccb2ff845e9
Marc Drewing <mldrewing@googlemail.com>
2022-12-18 16:33:08 +0100

    Added a reset button and changed variable names for better understandability

--- src/main/java/de/uni/hannover/swt/GUI/Control.java
+++ src/main/java/de/uni/hannover/swt/GUI/Control.java
@@ -4,6 +4,8 @@ import de.uni.hannover.swt.logic.IGame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -13,44 +15,63 @@ Adds the control to the MVC Pattern.
 
 public class Control extends JPanel {
 
-    private final View board; //The view which the control is connected to in the mvc pattern
-    private JLabel nextPlayerLabel;
+    private final View gameDrawing; //Drawing of the grid and the entries into the grid
+    private JLabel nextPlayerLabel; //Label that shows which players turn it is
+    private JButton resetButton; //Reset button to restart the game
+    private JPanel controlPanel; //Panel for the control of the game with reset button and information about the player turn
+    private JPanel gamePanel; //The big Panel that contains the whole UI and game
 
-    private final int WINDOW_WIDTH = 550, WINDOW_HEIGHT = 600;
+    private final int WINDOW_WIDTH = 550, WINDOW_HEIGHT = 600; //Preferences for window size of the game window
 
 
     public Control(View b) {
         setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
 
-        board = b; //The constructor takes the view to control as a parameter and saves it to the attributes of the object
+        gameDrawing = b; //The constructor takes the view to control as a parameter and saves it to the attributes of the object
+
+        controlPanel = new JPanel();
 
         nextPlayerLabel = new JLabel("nextPlayer:");
         setLayout(new BorderLayout());
-        add(nextPlayerLabel, BorderLayout.SOUTH);
-        nextPlayerLabel.setText(String.valueOf(board.getCurrentGame().getPlayer()));
+        controlPanel.add(nextPlayerLabel, BorderLayout.SOUTH);
+        nextPlayerLabel.setText(String.valueOf(gameDrawing.getCurrentGame().getPlayer()));
 
-        JPanel controlPanel = new JPanel();
+        resetButton = new JButton("Reset");
+        controlPanel.add(resetButton, BorderLayout.NORTH);
 
-
-        controlPanel.add(board); //adds the view to the panel
-        add(controlPanel);
-
+        gamePanel = new JPanel();
 
 
-        board.addMouseListener(new MouseAdapter() {
+        gamePanel.add(gameDrawing); //adds the view to the panel
+        gamePanel.add(controlPanel);
+
+        add(gamePanel);
+
+
+
+        gameDrawing.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                int c = (int) (e.getPoint().getX() / board.POS_SCALING_FACTOR);
-                int r = (int) (e.getPoint().getY() / board.POS_SCALING_FACTOR);
+                int c = (int) (e.getPoint().getX() / gameDrawing.POS_SCALING_FACTOR);
+                int r = (int) (e.getPoint().getY() / gameDrawing.POS_SCALING_FACTOR);
                 whenMousePressed(c, r);
+            }
+        });
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                IGame newGame = gameDrawing.getCurrentGame();
+                newGame.resetGame();
+                gameDrawing.setCurrentGame(newGame);
             }
         });
     }
 
     public void whenMousePressed(int c, int r) {
-        IGame game = board.getCurrentGame();
+        IGame game = gameDrawing.getCurrentGame();
         game.setPoint(r, c);
-        board.setCurrentGame(game);
+        gameDrawing.setCurrentGame(game);
         nextPlayerLabel.setText(String.valueOf(game.getPlayer())); //Takes the Player of the new Turn and changes the JLabel to show the name
     }
 }
